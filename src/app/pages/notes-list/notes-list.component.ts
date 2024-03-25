c5980e207095109d381f69989e57cf4208c31428
chenannchi <anchichen1209@gmail.com>
2024-03-25 10:41:36 +0800

    add a filter for noteslist

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Note } from '../../shared/note.model';
 import { NotesService } from '../../shared/notes.service';
 import { animate, stagger, style, transition, trigger, query } from '@angular/animations';
+
+export interface NoteCountObject {
+  [key: number]: number;
+}
 
 @Component({
   selector: 'app-notes-list',
@@ -52,17 +56,17 @@ import { animate, stagger, style, transition, trigger, query } from '@angular/an
     ]),
     trigger('listAnim',
       [
-        transition('* => *',[
-          query(':enter',[
+        transition('* => *', [
+          query(':enter', [
             style({
-              opacity:0,
-              height:0,
+              opacity: 0,
+              height: 0,
 
             }),
-            stagger(100,[
+            stagger(100, [
               animate('0.2s ease')
             ])
-          ],{optional:true})
+          ], { optional: true })
         ]
 
         )
@@ -70,16 +74,101 @@ import { animate, stagger, style, transition, trigger, query } from '@angular/an
     )
   ],
 })
+
+
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>()
+  filteredNotes: Note[] = new Array<Note>()
+
+  @ViewChild('filterInput') filterInputElRef!:ElementRef<HTMLInputElement> 
 
   constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
     this.notes = this.notesService.getAll()
+    // this.filteredNotes = this.notesService.getAll()
+    this.filter('')
+
   }
 
-  deleteNote(id: number) {
-    this.notesService.delete(id)
+  deleteNote(note:Note) {
+    let noteId = this.notesService.getId(note)
+    this.notesService.delete(noteId)
+    this.filter(this.filterInputElRef.nativeElement.value)
+  }
+
+  generateNoteURL(note:Note){
+    let noteId = this.notesService.getId(note)
+    return noteId.toString()
+  }
+
+  filter(query: string) {
+    if (!query.trim()) {
+      // If query is empty or only whitespace, show all notes
+      this.filteredNotes = this.notes;
+      return;
+    }
+
+    let allResults: Note[] = new Array<Note>()
+    query = query.toLowerCase().trim()
+    let terms: string[] = query.split('')
+    terms = this.removeDuplicates(terms)
+    terms.forEach(term => {
+      let results = this.relevantNotes(term)
+      allResults = [...allResults, ...results]
+    })
+
+    let uniqueResults = this.removeDuplicates(allResults)
+    // console.log("uniqueResults",uniqueResults);
+    this.filteredNotes = uniqueResults
+
+    this.sortByRelevancy(allResults)
+  }
+
+  removeDuplicates(arr: Array<any>): Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+    arr.forEach((e => uniqueResults.add(e)))
+
+    return Array.from(uniqueResults)
+  }
+
+  relevantNotes(query: string): Array<Note> {
+    query = query.toLowerCase().trim()
+    let relevantNotes = this.notes.filter(note => {
+      if (note.title && note.title.toLowerCase().includes(query)) {
+        return true
+      }else if(note.body && note.body.toLowerCase().includes(query)){
+        return true
+      }
+      return false
+    })
+
+    return relevantNotes
+  }
+
+  
+
+  sortByRelevancy(searchResults:Note[]){
+    let noteCountObj: NoteCountObject = {}
+    searchResults.forEach(note=>{
+      let noteId = this.notesService.getId(note)
+
+      if(noteCountObj[noteId]){
+        noteCountObj[noteId]+=1
+      }else{
+        noteCountObj[noteId]=1
+      }
+
+    })
+
+    this.filteredNotes = this.filteredNotes.sort((a:Note, b:Note)=>{
+      let aId = this.notesService.getId(a)
+      let bId = this.notesService.getId(b)
+
+      let aCount = noteCountObj[aId]
+      let bCount = noteCountObj[bId]
+
+      return bCount - aCount
+    })
   }
 }
